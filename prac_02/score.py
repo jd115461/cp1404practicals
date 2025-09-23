1c97aa965b109d68bf223959901eba0c79d5ed03
jd115461 <hpreston@wawarra.com>
2025-09-23 13:46:50 +1000

    Refactoring of score and incorporating random score code

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,18 +3,21 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
+from random import randint
 
 def main():
-    score = float(input("Enter score: "))
-    print(determine_result_status(score))
+    grade_score = float(input("Enter score: "))
+    print(determine_result_status(grade_score))
+    random_score = randint(0, 100)
+    print(f"Random score is: {random_score} ")
 
 
-def determine_result_status(score):
-    if score < 0 or score > 100:
+def determine_result_status(grade_score):
+    if grade_score < 0 or grade_score > 100:
         return "Invalid score"
-    elif score >= 90:
+    elif grade_score >= 90:
         return "Excellent"
-    elif score >= 50:
+    elif grade_score >= 50:
         return "Passable"
     else:
         return "Bad"
