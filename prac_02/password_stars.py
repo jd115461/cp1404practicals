80d7996f8ac3cec98a99f4af94722eb19d1b219b
jd115461 <hpreston@wawarra.com>
2025-09-23 13:06:54 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,21 @@
-password = input("Enter Password of at least 6 characters: ")
-while len(password) < 6:
-    print("Error, please try again!")
-    password = input("Enter Password of at least 6 characters: ")
-print("*" * len(password))
+MINIMUM_CHARACTERS = 6
 
+
+def main():
+    password = get_password(MINIMUM_CHARACTERS)
+    print_asterisks(password)
+
+
+def get_password(MINIMUM_CHARACTERS):
+    password = input(f"Enter password of at least {MINIMUM_CHARACTERS} characters: ")
+    while len(password) < MINIMUM_CHARACTERS:
+        print("Password too short")
+        password = input(f"Enter password of at least {MINIMUM_CHARACTERS} characters: ")
+    return password
+
+
+def print_asterisks(password):
+    print("*" * len(password))
+
+
+main()
