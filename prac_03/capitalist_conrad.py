c30283ee1734ed61d8190461a1081b1a877bb35a
jd115461 <hpreston@wawarra.com>
2025-09-30 12:58:50 +1000

    Program outputs results to file

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -14,10 +14,13 @@ MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 1.0
 MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
+FILENAME = "stock_simulation_output.txt"
+
+out_file = open(FILENAME, 'w')
 
 price = INITIAL_PRICE
 number_of_days = 0
-print(f"Starting price: ${price:,.2f}")
+print(f"Starting price: ${price:,.2f}", file=out_file)
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
@@ -33,6 +36,7 @@ while MIN_PRICE <= price <= MAX_PRICE:
         # between negative MAX_DECREASE and 0
         price_change = random.uniform(-MAX_DECREASE, 0)
 
-
     price *= (1 + price_change)
-    print(f"On day {number_of_days} price is: ${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+
+out_file.close()
