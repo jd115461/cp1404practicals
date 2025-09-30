2813f996c3c86733051fa7a00a613dde59854f4c
jd115461 <hpreston@wawarra.com>
2025-09-30 12:44:10 +1000

    Answered questions and wrote code for random number between 1 and 100

---
+++ prac_03/randoms.py
@@ -0,0 +1,10 @@
+# On line 1 the answer was 8
+# The smallest number could have been a 5 and the largest 20
+# On line 2 the  umber was 5
+# The smallest number could have been a 3 and the largest 10
+# No line 2 could not have produced a 4 as it is going up in intervals of 2
+# Line 3 gave 4.500757760042794
+# The smallest number could have been 2.5 and the largest 5.5
+
+import random
+print(random.randint(1, 100))
