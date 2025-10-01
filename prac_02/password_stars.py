2b130219f1248ace7e9fea0b04d5f00628402c60
jd115461 <hpreston@wawarra.com>
2025-10-01 11:09:06 +1000

    Modified feedback recived

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -6,7 +6,7 @@ def main():
     print_asterisks(password)
 
 
-def get_password(MINIMUM_CHARACTERS):
+def get_password(MINIMUM_CHARACTER):
     password = input(f"Enter password of at least {MINIMUM_CHARACTERS} characters: ")
     while len(password) < MINIMUM_CHARACTERS:
         print("Password is too short, please try again!")
