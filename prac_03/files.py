30b04e699b9367eb1d37a8ac8d13183215a4eb78
jd115461 <hpreston@wawarra.com>
2025-10-01 11:28:18 +1000

    Added code that prints the total of the numbers in number.txt file

--- prac_03/files.py
+++ prac_03/files.py
@@ -7,15 +7,29 @@ Make the appropriate choice of file-reading technique for each of these question
 - for line in file
 """
 
-# 1
+# # 1
 FILENAME = "name.txt"
 out_file = open(FILENAME, 'w')
 name = input("What is your name? ")
 print(name, file=out_file)
 out_file.close()
-
-# 2
+#
+# # 2
 in_file = open("name.txt", "r")
 name = in_file.read().strip()
 in_file.close()
 print(f"Hello, {name}!")
+
+# # 3
+with open("numbers.txt", "r") as in_file:
+    number_one = int(in_file.readline())
+    number_two = int(in_file.readline())
+print(number_one + number_two)
+
+# 4
+total = 0
+with open("numbers.txt", "r") as in_file:
+    for line in in_file:
+        number = int(line)
+        total += number
+    print(total)
