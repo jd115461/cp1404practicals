4606d247a465a9ccfe29afdc03036186a14f4e80
jd115461 <hpreston@wawarra.com>
2025-10-01 11:31:46 +1000

    Fixed comments

--- prac_03/files.py
+++ prac_03/files.py
@@ -7,20 +7,20 @@ Make the appropriate choice of file-reading technique for each of these question
 - for line in file
 """
 
-# # 1
+# 1
 FILENAME = "name.txt"
 out_file = open(FILENAME, 'w')
 name = input("What is your name? ")
 print(name, file=out_file)
 out_file.close()
-#
-# # 2
+
+# 2
 in_file = open("name.txt", "r")
 name = in_file.read().strip()
 in_file.close()
 print(f"Hello, {name}!")
 
-# # 3
+# 3
 with open("numbers.txt", "r") as in_file:
     number_one = int(in_file.readline())
     number_two = int(in_file.readline())
