e6dbc9ecb40a393fa1dba91b951f85857e731821
jd115461 <hpreston@wawarra.com>
2025-10-07 15:20:15 +1000

    Refactor quick pick program to use generate_quick_pick function instead of including in main

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -11,20 +11,24 @@ MAXIMUM = 45
 
 
 def main():
-    number_of_quick_picks = int(input("How many qick picks? "))
+    number_of_quick_picks = int(input("How many quick picks? "))
     while number_of_quick_picks < 0:
         print("That is an invalid number of picks! please try again")
         number_of_quick_picks = int(input("How many qick picks? "))
 
     for i in range(number_of_quick_picks):
-        quick_pick = []
-        for j in range(NUMBERS_PER_LINE):
-            number = random.randint(MINIMUM, MAXIMUM)
-            while number in quick_pick:
-                number = random.randint(MINIMUM, MAXIMUM)
-            quick_pick.append(number)
-        quick_pick.sort()
+        quick_pick = generate_quick_pick()
         print(" ".join(f"{number:2}" for number in quick_pick))
+
+
+def generate_quick_pick():
+    quick_pick = []
+    while len(quick_pick) < NUMBERS_PER_LINE:
+        number = random.randint(MINIMUM, MAXIMUM)
+        if number not in quick_pick:
+            quick_pick.append(number)
+    quick_pick.sort()
+    return quick_pick
 
 
 main()
