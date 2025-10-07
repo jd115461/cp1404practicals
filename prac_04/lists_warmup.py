b0eee5754d466028908472194db5903d9e935fba
jd115461 <hpreston@wawarra.com>
2025-10-07 12:21:15 +1000

    Add lists_warmup.py exercise with list slicing and updates

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -10,3 +10,8 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # "3" in numbers: False
 # numbers + [6, 5, 3]: [3, 1, 4, 1, 5, 9, 6, 5, 3]
 
+numbers[0] = "ten"
+numbers[-1] = 1
+print(numbers[2:])
+print(9 in numbers)
+
