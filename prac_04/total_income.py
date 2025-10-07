0efd3f9b15e8f38b0635024202fa0398130296d6
jd115461 <hpreston@wawarra.com>
2025-10-07 13:02:02 +1000

    Extract report printing into print_income_report function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -4,6 +4,15 @@ Starter code for cumulative total income program
 """
 
 
+def print_income_report(incomes, number_of_months):
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, number_of_months + 1):
+        income = incomes[month - 1]
+        total += income
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+
+
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
@@ -13,12 +22,7 @@ def main():
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
-    print("\nIncome Report\n-------------")
-    total = 0
-    for month in range(1, number_of_months + 1):
-        income = incomes[month - 1]
-        total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+    print_income_report(incomes, number_of_months)
 
 
 main()
