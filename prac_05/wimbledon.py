f55f9dffd6633e60e12f8142525cc1d22f30a10e
jd115461 <hpreston@wawarra.com>
2025-10-19 12:07:10 +1000

    Add more relevent names to functions and fix general code issues

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,5 +1,5 @@
 """
-Emails
+Wimbledon
 Estimate: 21 minutes 26 seconds 760 millisecond
 Actual:  22 minutes  16 seconds 450 millisecond
 """
@@ -12,29 +12,8 @@ INDEX_CHAMPION = 2
 def main():
     """Read the data file and print details about Wimbledon champions and countries"""
     records = load_records(FILENAME)
-    champion_to_count, countries = analyse_records(records)
-    display_results(champion_to_count, countries)
-
-
-def analyse_records(records):
-    champion_to_count = {}
-    countries = set()
-    for record in records:
-        countries.add(record[INDEX_COUNTRY])
-        try:
-            champion_to_count[record[INDEX_CHAMPION]] += 1
-        except KeyError:
-            champion_to_count[record[INDEX_CHAMPION]] = 1
-    return champion_to_count, countries
-
-
-def display_results(champion_to_count, countries):
-    """Display record results as champions and countries"""
-    print("Wimbledon Champions: ")
-    for name, count in champion_to_count.items():
-        print(name, count)
-    print(f"\nThese {len(countries)} countries have won Wimbledon: ")
-    print(", ".join(sorted(countries)))
+    champion_to_date, countries = analyse_records(records)
+    display_results(champion_to_date, countries)
 
 
 def load_records(filename):
@@ -45,7 +24,29 @@ def load_records(filename):
         for line in in_file:
             parts = line.strip().split(",")
             records.append(parts)
-        return records
+    return records
+
+
+def analyse_records(records):
+    """Create a dictionary of champions to date"""
+    champion_to_date = {}
+    countries = set()
+    for record in records:
+        countries.add(record[INDEX_COUNTRY])
+        try:
+            champion_to_date[record[INDEX_CHAMPION]] += 1
+        except KeyError:
+            champion_to_date[record[INDEX_CHAMPION]] = 1
+    return champion_to_date, countries
+
+
+def display_results(champion_to_date, countries):
+    """Display record results as champions and countries"""
+    print("Wimbledon Champions: ")
+    for name, count in champion_to_date.items():
+        print(name, count)
+    print(f"\nThese {len(countries)} countries have won Wimbledon: ")
+    print(", ".join(sorted(countries)))
 
 
 main()
