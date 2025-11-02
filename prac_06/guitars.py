fe423e68ba9b44cdc3e3b59edca1601e704fcff1
jd115461 <hpreston@wawarra.com>
2025-11-02 11:26:14 +1000

    Add formatted display, and test data option for guitars.py file

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -6,17 +6,36 @@ Actual time: approx  minutes
 """
 from prac_06.guitar import Guitar
 
+
 def main():
     """Guitar program using the created Guitar class"""
     guitars = []
 
     print("My guitars!")
-    name = input("Name: ")
+    name = input("Name: ").strip()
+
     while name != "":
         year = int(input("Year: "))
-        cost = int(input("Cost: $"))
+        cost = float(input("Cost: $"))
         add_created_guitar = Guitar(name, year, cost)
-        print(add_created_guitar, "added")
-        name = input("Name: ")
+        guitars.append(add_created_guitar)
+        print(f"{add_created_guitar} added.\n")
+
+        name = input("Name: ").strip()
+
+    # Lines for testing code manually
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    if guitars:
+        print("These are my guitars:")
+        for i, guitar in enumerate(guitars, 1):
+            vintage_string = ""
+            if guitar.is_vintage():
+                vintage_string = " (vintage)"
+            print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+    else:
+        print("No guitars :( Quick, go and buy one!")
 
 
+main()
