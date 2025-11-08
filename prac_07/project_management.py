8b380d631ecf22c5b37aa5e3c87f9f6ec004582b
jd115461 <hpreston@wawarra.com>
2025-11-08 20:13:52 +1000

    Add Menu setup and start main() function.

---
+++ prac_07/project_management.py
@@ -0,0 +1,23 @@
+"""
+CP1404/CP5632 Practical 7 - Project Management Program task
+
+Estimated time: 2 hours
+Actual time:
+"""
+
+import datetime
+from project import Project
+
+FILENAME = "projects.txt"
+
+MENU = """- (L)oad projects
+- (S)ave projects
+- (D)isplay projects
+- (F)ilter projects by date
+- (A)dd new project
+- (U)pdate project
+- (Q)uit"""
+
+
+def main():
+    pass
